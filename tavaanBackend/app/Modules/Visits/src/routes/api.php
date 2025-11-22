565bc8cf9ce17dd9e0c5e22bfb453ae3117f547e
aminsaki <aminsaki69@gmail.com>
2025-11-22 14:23:17 -0800

    copde

--- tavaanBackend/app/Modules/Visits/src/routes/api.php
+++ tavaanBackend/app/Modules/Visits/src/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use  holoo\modules\Visits\Https\Controllers as Controller;
 
-Route::middleware('auth:api')->group(function () {
+//Route::middleware('auth:api')->group(function () {
 
 Route::apiResource('visits', Controller\VisitController::class);
 
@@ -11,4 +11,4 @@ Route::post('serachVisits', [Controller\VisitController::class, 'serachVisits'])
 Route::post('updateVisits', [Controller\VisitController::class, 'updateVisits']);
 Route::post('visits/export-excel', [Controller\VisitController::class, 'exportExcel']);
 
-});
+//});
